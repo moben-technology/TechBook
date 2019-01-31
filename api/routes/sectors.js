5e25308e692d33b5d8e1674dd4cc967bcca547f0
mohamedbenaissa93 <mohamed.benaissa.ing@gmail.com>
2019-01-31 13:27:34 +0100

    get list sectors with nbr occuences of publications

--- api/routes/sectors.js
+++ api/routes/sectors.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Sector = require('../models/sector');
-
-//get all sectors
+const Publication = require('../models/publication').publicationModel;
+var async = require('async');
+// //get all sectors
 router.get('/getAllSectors', function (req, res) {
     try {
         Sector.find(function (err, sectors) {
@@ -14,6 +15,8 @@ router.get('/getAllSectors', function (req, res) {
                 });
 
             } else {
+                // return nbr publications for each sector
+
                 res.json({
                     status: 1,
                     message: 'get list sectors successfully',
@@ -26,6 +29,94 @@ router.get('/getAllSectors', function (req, res) {
             }
 
         });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            status: 0,
+            message: '500 Internal Server Error',
+            data: {}
+        })
+
+    }
+});
+
+//get all sectors
+router.get('/getAllSectorsWithNbrOccurences', function (req, res) {
+    try {
+        async.series([
+            // return the occurences of each sector in all publications
+                function (callback) {
+                    try {
+                         Publication.aggregate([
+                             {"$group": {"_id": "$sector", "count" : {$sum : 1}}}
+                         ]).exec(function (err, resultOccurence) {
+                            callback(null, resultOccurence);
+                         })
+                    } catch (err) {
+                        console.log(err);
+                        res.json({
+                            status: 0,
+                            message: '500 Internal Server Error',
+                            data: {}
+                        })
+
+                    }
+                },
+                // return all sector in dataBase
+                function (callback) {
+                    Sector.find(function (err, sectors) {
+                        if (err) {
+                            res.json({
+                                status: 0,
+                                message: ('error get list sectors.' + err)
+            
+                            });
+            
+                        } else {
+                            callback(null, sectors);
+                        }
+            
+                    });
+                }
+            ],
+            // optional callback
+            function (err, results) {
+                if (err) {
+                    return res.json({
+                        status: 0,
+                        message: ('Error while getting publications') + err
+                    });
+
+                } else {
+                    var arrayOccurance =  results[0];
+                    var arrayAllSectors = results[1];
+                    var resultArray = [];
+
+                    arrayAllSectors.forEach(sector => {
+                        var count = 0;
+                        index = arrayOccurance.map(function(e) { 
+                            return (e._id).toString(); }).indexOf((sector._id).toString());
+                        if (index != -1) {
+                            count = arrayOccurance[index].count;
+                        }
+                        var element = {
+                            "_id":sector._id,
+                            "nameSector":sector.nameSector,
+                            "count":count,
+                        }
+                        resultArray.push(element)
+                    });
+
+                    res.json({
+                        status: 1,
+                        data: {
+                            sectors: resultArray,
+                           
+                        }
+
+                    });
+                }
+            });
     } catch (err) {
         console.log(err);
         res.json({
